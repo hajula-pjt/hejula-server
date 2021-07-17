546e422239cc6efcff16bbafe33285d779b1273a
JooYeonPark17 <jyeon0117@naver.com>
2021-07-17 09:53:50 +0900

    init
    initialization

---
+++ src/main/java/com/hejula/hejulaserver/HejulaServerApplication.java
@@ -0,0 +1,13 @@
+package com.hejula.hejulaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HejulaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HejulaServerApplication.class, args);
+	}
+
+}
